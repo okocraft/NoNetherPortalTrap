0430355c977fc2b6016af143106f0478d253ae8b
Siroshun09 <siro.shun8@gmail.com>
2023-06-09 02:05:02 +0900

    build: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     java
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
 }
 
 java.toolchain.languageVersion.set(JavaLanguageVersion.of(17))
@@ -15,14 +15,13 @@ repositories {
 }
 
 dependencies {
-    compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+    compileOnly("org.spigotmc:spigot-api:1.20-R0.1-SNAPSHOT")
+    compileOnly("org.jetbrains:annotations:24.0.1")
 
-    implementation("org.jetbrains:annotations:23.0.0")
+    implementation("net.kyori:adventure-api:4.14.0")
+    implementation("net.kyori:adventure-platform-bukkit:4.3.0")
 
-    implementation("net.kyori:adventure-api:4.12.0")
-    implementation("net.kyori:adventure-platform-bukkit:4.1.2")
-
-    implementation("com.github.siroshun09.configapi:configapi-yaml:4.6.0")
+    implementation("com.github.siroshun09.configapi:configapi-yaml:4.6.4")
     implementation("com.github.siroshun09.translationloader:translationloader:2.0.2")
 }
 
@@ -43,5 +42,3 @@ tasks {
         relocate("com.github.siroshun09", "${group}.${name.toLowerCase()}.lib")
     }
 }
-
-
